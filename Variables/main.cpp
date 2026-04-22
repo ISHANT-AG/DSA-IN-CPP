76421301054cdef235d9a04d7f8d657b0edd728e
ISHANT-AG <agarwalishant690@gmail.com>
2026-04-22 07:30:15 +0530

    ishant:

---
+++ Variables/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    // Variable declaration and initialization
+    int age = 25; // Integer variable
+    cout << "My age is: " << age << endl;
+
+    return 0;
+}
